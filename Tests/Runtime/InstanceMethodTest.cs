3662a59831b6131c57b1d72aba3223698f635117
Klaudijus <klaudmise@gmail.com>
2021-03-24 02:01:06 +0000

    Added some simple test methods for future attributes work.

---
+++ Tests/Runtime/InstanceMethodTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceMethodTest
+{
+    public void EmptyMethod()
+    {
+
+    }
+
+    public int ReturnMethod()
+    {
+        return 10;
+    }
+
+    public void ParamMethod(int a)
+    {
+
+    }
+
+    public void OverloadedMethod(int a)
+    {
+
+    }
+
+    public void OverloadedMethod(int a, int b)
+    {
+
+    }
+
+    public void OutParamMethod(int a, out string b)
+    {
+        b = "Output String";
+    }
+
+    public void OptionalParamMethod(int a, int b = 0, string c = "")
+    {
+
+    }
+}
---
+++ Tests/Runtime/StaticMethodTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticMethodTest : MonoBehaviour
+{
+    public static void EmptyMethod()
+    {
+
+    }
+
+    public static int ReturnMethod()
+    {
+        return 10;
+    }
+
+    public static void ParamMethod(int a)
+    {
+
+    }
+
+    public static void OverloadedMethod(int a)
+    {
+
+    }
+
+    public static void OverloadedMethod(int a, int b)
+    {
+
+    }
+
+    public static void OutParamMethod(int a, out string b)
+    {
+        b = "Output String";
+    }
+
+    public static void OptionalParamMethod(int a, int b = 0, string c = "")
+    {
+
+    }
+}
