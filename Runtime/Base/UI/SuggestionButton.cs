894f9f9e621dc4865147a9b550e2f8f33e6873e4
Klaudijus <klaudmise@gmail.com>
2021-06-05 15:23:19 +0100

    Amended logic.

---
+++ Runtime/Base/UI/SuggestionButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuggestionButton : MonoBehaviour
+{
+    [SerializeField]
+    private Text _CommandKeyText;
+
+    [SerializeField]
+    private Text _CommandDescText;
+
+    public Text CommandKeyText => _CommandKeyText;
+
+    public Text CommandDescText => _CommandDescText;
+}
