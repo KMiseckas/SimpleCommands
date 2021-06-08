085340f82a1e5f60554cb2b695e7661265798824
Klaudijus <klaudmise@gmail.com>
2021-06-09 00:31:09 +0100

    Added comments.

--- Runtime/Base/ParserMethods.cs
+++ Runtime/Base/ParserMethods.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Parser methods for several different types of instances.
+/// </summary>
 public static class ParserMethods
 {
+    /// <summary>
+    /// Parse string to return a Vector2.
+    /// </summary>
     public static Vector2 ParseVector2(string s)
     {
         string[] values = s.Split(',');
@@ -11,6 +17,9 @@ public static class ParserMethods
         return new Vector2(float.Parse(values[0]), float.Parse(values[1]));
     }
 
+    /// <summary>
+    /// Parse string to return a Vector3.
+    /// </summary>
     public static Vector3 ParseVector3(string s)
     {
         string[] values = s.Split(',');
@@ -18,6 +27,9 @@ public static class ParserMethods
         return new Vector3(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2]));
     }
 
+    /// <summary>
+    /// Parse string to return a Vector4.
+    /// </summary>
     public static Vector4 ParseVector4(string s)
     {
         string[] values = s.Split(',');
@@ -25,6 +37,9 @@ public static class ParserMethods
         return new Vector4(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2]), float.Parse(values[3]));
     }
 
+    /// <summary>
+    /// Parse string to return a Rect.
+    /// </summary>
     public static Rect ParseRect(string s)
     {
         string[] values = s.Split(',');
@@ -32,6 +47,9 @@ public static class ParserMethods
         return new Rect(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2]), float.Parse(values[3]));
     }
 
+    /// <summary>
+    /// Parse string where the string represents the runtime ID of the gameobject to find and return.
+    /// </summary>
     public static GameObject ParseGORuntimeID(string s)
     {
         int intID = int.Parse(s);
@@ -50,6 +68,9 @@ public static class ParserMethods
         return null;
     }
 
+    /// <summary>
+    /// Parse string where the string represents the name of a GameObject to find and return.
+    /// </summary>
     public static GameObject ParseGOByName(string s)
     {
         return GameObject.Find(s);
